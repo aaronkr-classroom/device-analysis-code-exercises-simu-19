a1480cdbc3c577a05779224a28a7573062894ebe
simu-19 <siammymen98@gmail.com>
2026-04-02 15:13:57 +0900

    Create april 02 hw 5.py

---
+++ april 02 hw 5.py
@@ -0,0 +1,11 @@
+a= 132  #Ob10000100
+b=45  #Ob00101101
+fmt0='{:<10}'
+fmt1='Ob{:08b} 0x{:02x} {:3}'
+n=30
+
+print("bitwise left shift:")
+print(fmt0.format('a'),fmt1.format(a,a,a))
+#print(fmt0.format('b'),fmt1.format(b,b,b))
+print('-'*n)
+print(fmt0.format('a<<2'),fmt1.format(a<<2&0xFF,a<<2&0xFF,a<<2&0xFF))
