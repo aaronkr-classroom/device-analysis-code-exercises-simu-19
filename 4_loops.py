a27ecd6233e0bd90b7a0e9fd72458b0e062b68be
simu-19 <siammymen98@gmail.com>
2026-04-02 15:10:34 +0900

    Create 4_loops.py

---
+++ 4_loops.py
@@ -0,0 +1,17 @@
+#4_loops.py
+
+#math
+print(10+3)
+print(10-3)
+print(10*3)
+print(10/3)
+print(10%3)
+print(10//3)
+print(10**3)
+print(10==3)
+print(10!=3)
+
+print(10>=3)
+print(10<3)
+print(10<3 or 3<10)
+print(not 10<3)
