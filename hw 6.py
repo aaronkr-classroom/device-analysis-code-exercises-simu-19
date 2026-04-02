8bf473403b755c70b0972492f58b514d646ac481
simu-19 <siammymen98@gmail.com>
2026-04-02 15:17:04 +0900

    Create hw 6.py

---
+++ hw 6.py
@@ -0,0 +1,12 @@
+#rightshift
+a= 132  #Ob10000100
+b=45  #Ob00101101
+fmt0='{:<10}'
+fmt1='Ob{:08b} 0x{:02x} {:3}'
+n=30
+
+print("bitwise right shift:")
+print(fmt0.format('a'),fmt1.format(a,a,a))
+#print(fmt0.format('b'),fmt1.format(b,b,b))
+print('-'*n)
+print(fmt0.format('a>>2'),fmt1.format(a>>2&0xFF,a>>2&0xFF,a>>2&0xFF))
