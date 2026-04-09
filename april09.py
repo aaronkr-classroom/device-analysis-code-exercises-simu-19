6cd289d1d187123dd564f1261e76faa76446a5bb
simu-19 <siammymen98@gmail.com>
2026-04-09 15:00:47 +0900

    Create april09.py

---
+++ april09.py
@@ -0,0 +1,19 @@
+data_types.py
+
+a = 10 # int
+
+ b = 3.14 # float
+
+ c = "Hello" # str
+
+d = True # bool
+
+
+
+ print(f"{a} is a {type(a)}")
+
+ print(f"{b} is a {type(b)}")
+
+print(f"{c} is a {type(c)}")
+
+print(f"{d} is a {type(d)}
